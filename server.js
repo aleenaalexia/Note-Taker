68765d9197c5a945b5710a7bdee50336f1a309a3
aleena <aleenajohns31@gmail.com>
2022-09-23 21:12:08 -0700

    server running on port 3001

---
+++ server.js
@@ -0,0 +1,14 @@
+const express = require('express');
+
+const app = express();
+const PORT = process.env.PORT || 3001;
+
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+app.use(express.static(__dirname));
+
+require('./routes')(app);
+
+app.listen(PORT, () => {
+    console.log(`Server running on port ${PORT}`);
+});
